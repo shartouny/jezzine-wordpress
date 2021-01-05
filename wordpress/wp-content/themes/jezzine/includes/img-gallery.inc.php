312a713395a9269b6c1fead4eb595d207d078b0b
shartouny <shartouny@outlook.com>
2021-01-05 11:15:09 +0200

    m

--- wordpress/wp-content/themes/jezzine/includes/img-gallery.inc.php
+++ wordpress/wp-content/themes/jezzine/includes/img-gallery.inc.php
@@ -2,13 +2,21 @@
 <section id="img-gallery" class="bg-light-gray mt-md-5 mt-2 p-md-5 p-2">
     <div class="container">
         <h2 class="middle-line">
-            <span class="text-right"> ألبوم الصور </span>
+            <span class="text-right"> <?php echo _e('ألبوم الصور') ;?></span>
         </h2>
         <div class="owl-carousel owl-theme mt-md-5 mt-2">
-            <div class="item"><img src="<?php echo get_stylesheet_directory_uri();?>/assets/pictures/gallery-img-1.png" alt="" class="img-fluid"></div>
-            <div class="item"><img src="<?php echo get_stylesheet_directory_uri();?>/assets/pictures/gallery-img-2.png" alt="" class="img-fluid"></div>
-            <div class="item"><img src="<?php echo get_stylesheet_directory_uri();?>/assets/pictures/gallery-img-3.png" alt="" class="img-fluid"></div>
-            <div class="item"><img src="<?php echo get_stylesheet_directory_uri();?>/assets/pictures/gallery-img-3.png" alt="" class="img-fluid"></div>
+            <?php 
+            $args = array(
+                'post_type' => 'img-gallery',
+                'post_status' => 'publish'
+            );
+            $imgs = new WP_Query($args);
+            while($imgs->have_posts()): $imgs->the_post();
+             ?>
+            <div class="item"><img src="<?php echo get_the_post_thumbnail_url(get_the_id());?>" alt="" class="img-fluid"></div>
+            <?php 
+            endwhile; wp_reset_postdata();
+            ?>
         </div>
     </div>
 </section>
