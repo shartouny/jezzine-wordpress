4353b08990b41aa3fe2502ce4f6c50ff25c952b3
shartouny <shartouny@outlook.com>
2021-01-06 14:33:56 +0200

    auto-commit

--- wordpress/wp-content/themes/jezzine/functions.php
+++ wordpress/wp-content/themes/jezzine/functions.php
@@ -30,98 +30,107 @@ add_theme_support('post-thumbnails');
 // 	return $atts;
 //   }
 //   add_filter( 'nav_menu_link_attributes', 'add_link_atts');
-function bootstrap_pagination(\WP_Query $wp_query = null, $echo = true,
-	$params = []
-) {
-	if (null === $wp_query) {
-		global $wp_query;
-	}
+// function bootstrap_pagination(\WP_Query $wp_query = null, $echo = true, $params = []) {
+// 	if (null === $wp_query) {
+// 		global $wp_query;
+// 	}
 
-	$add_args = [];
+// 	$add_args = [];
 
-	//add query (GET) parameters to generated page URLs
-	/*if (isset($_GET[ 'sort' ])) {
-        $add_args[ 'sort' ] = (string)$_GET[ 'sort' ];
-    }*/
+// 	//add query (GET) parameters to generated page URLs
+// 	/*if (isset($_GET[ 'sort' ])) {
+//         $add_args[ 'sort' ] = (string)$_GET[ 'sort' ];
+//     }*/
 
-	$pages = paginate_links(
-			array_merge([
-				// 'base'         => str_replace(
-				// 	999999999,
-				// 	'%#%',
-				// 	esc_url(get_pagenum_link(999999999))
-				// ),
-				'format'       => '?paged=%#%',
-				'current'      => max(1, get_query_var('paged')),
-				'total'        => $wp_query->max_num_pages,
-				'type'         => 'array',
-				'show_all'     => false,
-				'end_size'     => 2,
-				'mid_size'     => 1,
-				'prev_next'    => true,
-				'prev_text'    => __('الصفحة السابقة'),
-				'next_text'    => __('الصفحة التالية'),
-				'add_args'     => $add_args,
-				'add_fragment' => ''
-			], $params)
-		);
+// 	$pages = paginate_links(
+// 			array_merge([
+// 				// 'base'         => str_replace(
+// 				// 	999999999,
+// 				// 	'%#%',
+// 				// 	esc_url(get_pagenum_link(999999999))
+// 				// ),
+// 				'format'       => '?paged=%#%',
+// 				'current'      => max(1, get_query_var('paged')),
+// 				'total'        => $wp_query->max_num_pages,
+// 				'type'         => 'array',
+// 				'show_all'     => false,
+// 				'end_size'     => 2,
+// 				'mid_size'     => 1,
+// 				'prev_next'    => true,
+// 				'prev_text'    => __('الصفحة السابقة'),
+// 				'next_text'    => __('الصفحة التالية'),
+// 				'add_args'     => $add_args,
+// 				'add_fragment' => ''
+// 			], $params)
+// 		);
 
-	if (is_array($pages)) {
-		//$current_page = ( get_query_var( 'paged' ) == 0 ) ? 1 : get_query_var( 'paged' );
-		$pagination = '<ul class="pagination justify-content-center">';
+// 	if (is_array($pages)) {
+// 		//$current_page = ( get_query_var( 'paged' ) == 0 ) ? 1 : get_query_var( 'paged' );
+// 		$pagination = '<ul class="pagination justify-content-center">';
 
-		foreach ($pages as $page) {
-			$pagination .= '<li class="page-item' . (strpos($page, 'current') !== false ? ' active' : '') . '"> ' . str_replace('page-numbers', 'page-link', $page) . '</li>';
-		}
+// 		foreach ($pages as $page) {
+// 			$pagination .= '<li class="page-item' . (strpos($page, 'current') !== false ? ' active' : '') . '"> ' . str_replace('page-numbers', 'page-link', $page) . '</li>';
+// 		}
 
-		$pagination .= '</ul></div>';
+// 		$pagination .= '</ul></div>';
 
-		if ($echo) {
-			echo $pagination;
-		} else {
-			return $pagination;
-		}
-	}
+// 		if ($echo) {
+// 			echo $pagination;
+// 		} else {
+// 			return $pagination;
+// 		}
+// 	}
 
-	return null;
-}
+// 	return null;
+// }
 
-function twenty_twenty_one_widgets_init()
-{
+// function twenty_twenty_one_widgets_init()
+// {
 
-	register_sidebar(
-		array(
-			'name'          => esc_html__('Footer 1', 'twentytwentyone'),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__('Add widgets here to appear in your footer.', 'twentytwentyone'),
-			//'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			//'after_widget'  => '</section>',
-			//'before_title'  => '<h2 class="widget-title">',
-			//'after_title'   => '</h2>',
-		)
-	);
-	register_sidebar(
-		array(
-			'name'          => esc_html__('Footer 2', 'twentytwentyone'),
-			'id'            => 'sidebar-2',
-			'description'   => esc_html__('Add widgets here to appear in your footer.', 'twentytwentyone'),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h5 class="">',
-			'after_title'   => '</h5>',
-		)
-	);
-	register_sidebar(
-		array(
-			'name'          => esc_html__('Footer 3', 'twentytwentyone'),
-			'id'            => 'sidebar-3',
-			'description'   => esc_html__('Add widgets here to appear in your footer.', 'twentytwentyone'),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h5 class="">',
-			'after_title'   => '</h5>',
-		)
-	);
-}
-add_action('widgets_init', 'twenty_twenty_one_widgets_init');
+// 	register_sidebar(
+// 		array(
+// 			'name'          => esc_html__('Footer 1', 'twentytwentyone'),
+// 			'id'            => 'sidebar-1',
+// 			'description'   => esc_html__('Add widgets here to appear in your footer.', 'twentytwentyone'),
+// 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+// 			'after_widget'  => '</section>',
+// 			'before_title'  => '<h2 class="widget-title">',
+// 			'after_title'   => '</h2>',
+// 		)
+// 	);
+// 	register_sidebar(
+// 		array(
+// 			'name'          => esc_html__('Footer 2', 'twentytwentyone'),
+// 			'id'            => 'sidebar-2',
+// 			'description'   => esc_html__('Add widgets here to appear in your footer.', 'twentytwentyone'),
+// 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+// 			'after_widget'  => '</section>',
+// 			'before_title'  => '<h5 class="">',
+// 			'after_title'   => '</h5>',
+// 		)
+// 	);
+// 	register_sidebar(
+// 		array(
+// 			'name'          => esc_html__('Footer 3', 'twentytwentyone'),
+// 			'id'            => 'sidebar-3',
+// 			'description'   => esc_html__('Add widgets here to appear in your footer.', 'twentytwentyone'),
+// 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+// 			'after_widget'  => '</section>',
+// 			'before_title'  => '<h5 class="">',
+// 			'after_title'   => '</h5>',
+// 		)
+// 	);
+// 	register_sidebar(
+// 		array(
+// 			'name'          => esc_html__('contact-info', 'twentytwentyone'),
+// 			'id'            => 'sidebar-4',
+// 			'description'   => esc_html__('Add widgets here to appear in your footer.', 'twentytwentyone'),
+// 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+// 			'after_widget'  => '</section>',
+// 			'before_title'  => '<h5 class="">',
+// 			'after_title'   => '</h5>',
+// 		)
+// 	);
+// }
+// add_action('widgets_init', 'twenty_twenty_one_widgets_init');
 ?>
